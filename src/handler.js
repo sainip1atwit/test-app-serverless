0391d283fe88eb24a979002cb8fdc690bcb6e862
sainip1atwit <sainip1@wit.edu>
2025-01-27 18:24:48 -0500

    initial commit

---
+++ src/handler.js
@@ -0,0 +1,28 @@
+const serverless = require("serverless-http");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from root!",
+  });
+});
+
+app.get("/hello", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from path!",
+  });
+});
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+//server-full app
+// app.listen(3000, () => {
+//   console.log('Running on localhost:3000');
+// });
+
+exports.handler = serverless(app);
